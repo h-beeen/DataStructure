a489d0773d314e3d075c2975bb0730f120e84e35
Haebin <hbyeon@student.42seoul.kr>
2023-04-12 14:21:14 +0900

    add : Initial Commit

---
+++ 자료구조론_1stSubject.c
@@ -0,0 +1,24 @@
+// 경제학부 201811210 변해빈
+// 자료구조론 과제 제출
+
+#include <stdio.h>
+
+void hanoi(int n, char dst, char tmp, char to) {
+    if(n == 1)
+        printf("원판 1: %c -> %c\n", dst, to);
+    else {
+        for(int i=1; i<n; i++) {
+            hanoi(i, dst, to, tmp);
+            printf("원판 %d: %c -> %c\n", n, dst, to);
+            hanoi(i, tmp, dst, to);
+        }
+    }
+}
+
+double power(double x, int n){
+    int i;
+    double result 1.0;
+    for(int i = 0; i < n; i++)
+        result *= x;
+    return result;
+}
